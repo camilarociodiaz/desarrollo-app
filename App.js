e6389bdef6d2d7470a4cad8b0488543da9c3c877
Camila Diaz <camilarociodiaz@gmail.com>
2022-07-13 15:29:38 -0300

    separacion dearquitectura

---
+++ App.js
@@ -0,0 +1,78 @@
+import { StyleSheet, View, TextInput, Text, Button, FlatList, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react'
+import CustomModal from './components/Modal'
+import AddItem from './components/AddItem'
+import List from './components/List'
+
+export default function App( ) {
+    
+  const [textItem, setTextItem] = useState('');
+  const [itemList, setItemList] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [itemSelected, setItemSelected] = useState({});
+
+  const onHandlerChangeItem = (text) => setTextItem(text)
+  const onHandlerAddItem = () => {
+    console.log('se agrego el item', textItem)
+    setItemList(currentItems => [...currentItems, { id: Date.now(), value: textItem, completed: false}])
+    // setItemList({...itemList, id: Math.random()*10, value: textItem }) => hace lo mismo que la de arriba
+    setTextItem('')
+  }
+
+  const onHandlerDeleteItem = id => {
+    setItemList(currentItems => currentItems.filter(item => item.id !== id))
+    setItemSelected({})
+    setModalVisible(!modalVisible)
+  }
+  const onHandlerModal = id => {
+    setItemSelected(itemList.find(item => item.id === id))
+    setModalVisible(!modalVisible)
+  }
+
+  const onHandlerCompleteItem = id => {
+    let itemCompleted = itemList.findIndex((item) => item.id === id)
+    itemList[itemCompleted].completed = true
+    setItemList([...itemList])
+    setModalVisible(!modalVisible)
+  }
+  
+  // useEffect(() => {
+  //   // se ejectua todo el tiempo, NO SE USA ASI
+  // })
+    
+  // useEffect(() => {
+  //   // se ejecuta cuando se carga el componente
+  // }, [])
+  
+  // useEffect(() => {
+  //     // se ejecuta cuando se cambia el estado ITEMLIST
+  //     console.table(itemList)
+  //   }, [itemList])
+      
+  return (
+    <View style={styles.screen}>
+      <CustomModal 
+        modalVisible={modalVisible}
+        onHandlerDeleteItem={onHandlerDeleteItem}
+        itemSelected={itemSelected}
+        onHandlerCompleteItem={onHandlerCompleteItem}
+      /> 
+      <AddItem 
+        textItem={textItem}
+        onHandlerAddItem={onHandlerAddItem}
+        onHandlerChangeItem={onHandlerChangeItem}
+      />
+      <List 
+        itemList={itemList}
+        onHandlerModal={onHandlerModal}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    marginTop: '10%',
+    padding: 30,
+  },
+})
